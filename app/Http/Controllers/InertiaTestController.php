5408e49fd8e35bb6c0a815abdcca182910382dd4
iwafuchi <rta375@gmail.com>
2022-11-08 22:31:37 +0900

    v-for test

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\InertisaTest;
 use App\Http\Requests\InertiaTestStoreRequest;
 
 class InertiaTestController extends Controller {
     public function index() {
-        return Inertia::render('Inertia/Index');
+        return Inertia::render('Inertia/Index', [
+            'blogs' => InertisaTest::all()
+        ]);
     }
     public function create() {
         return Inertia::render('Inertia/Create');
