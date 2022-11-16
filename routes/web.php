a4ecb6b60a2c0ddfb46003e91779aaca0841e4eb
iwafuchi <rta375@gmail.com>
2022-11-16 17:29:03 +0900

    fix file path and uri

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
 });
 
-Route::get('/inertia/slot', function () {
-    return Inertia::render('Inertia/Slot');
+Route::get('/inertia-slot', function () {
+    return Inertia::render('InertiaSlot');
 });
 
 Route::get('/inertia/index', [InertiaTestController::class, 'index'])
