e8b62067de0dba8bee008f81ad480afd08e5d1e8
iwafuchi <rta375@gmail.com>
2022-11-20 00:05:34 +0900

    set route items

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
-{
+class Item extends Model {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string, boolean>
+     */
+    protected $fillable = [
+        'name',
+        'memo',
+        'price',
+        'is_selling'
+    ];
 }
